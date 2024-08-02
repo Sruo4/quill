89202ddc7e40e6cd2b85bc66f011d25768f15949
fanhe <hee.fan@outlook.com>
2024-08-02 21:37:09 +0800

    feature: landing  page添加Navbar, 接入kinde

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
     <html lang="en" className="light">
       <body className={cn(
         'min-h-screen font-sans antialiased grainy', inter.className
-      )}>{children}</body>
+      )}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   );
 }
